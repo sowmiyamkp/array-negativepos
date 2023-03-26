03e1ed604112d13289147ba773b4e533077558c0
sowmiyamkp <sowmiyamkp1305@gmail.com>
2023-03-25 19:32:23 -0700

    add array-negativepos

---
+++ main/main.js
@@ -0,0 +1,22 @@
+/*let d=[5,-3,2,7,10,-11],e=d[0],f=[0];
+for(let i=0;i<d.length;i++)
+{
+		if(e<d[i])
+		{
+			e=d[i];
+		}
+}
+			console.log(e);*/
+			
+			
+ var array=[10,19,32,2],e=array[0];
+ for(i=1;i<array.length;i++)
+ {	 if(e<array[i])
+
+	 {
+		 	  e=array[i];
+
+	 }
+	 console.log(e);
+ }
+	
